09314d9fc74374cb8392c9ff75bc6007616018b8
Олеся <o.d.dorosheva2007@mail.ru>
2025-11-27 00:34:25 +0300

    Practise 3

--- практическая 3/5.py
+++ практическая 3/5.py
@@ -2,4 +2,4 @@ shopping_list = ["молоко", "хлеб", "пельмени", "сахар"]
 shopping_list.append("соль")
 print(shopping_list)
 unique_items = set(shopping_list)
-print(unique_items)
+print(unique_items) #list - упорядоченная коллекция элементов, а set - неупорядоченная
